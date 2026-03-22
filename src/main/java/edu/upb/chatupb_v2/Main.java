2e1d9cc0d4a78029894460f1957331bbb9a84403
Alan V <villavicencioalan706@gmail.com>
2026-03-22 18:23:06 -0400

    PROYECTO

--- src/main/java/edu/upb/chatupb_v2/Main.java
+++ src/main/java/edu/upb/chatupb_v2/Main.java
@@ -3,11 +3,13 @@ package edu.upb.chatupb_v2;
 import edu.upb.chatupb_v2.model.network.ChatServer;
 import edu.upb.chatupb_v2.controller.Mediator;
 import edu.upb.chatupb_v2.controller.ProfileController;
+import edu.upb.chatupb_v2.model.repository.ConnectionDB;
 import edu.upb.chatupb_v2.view.JUi;
 import edu.upb.chatupb_v2.view.LoginDialog;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        ConnectionDB.initDataBase();
         String userId = UserIdentity.loadOrCreateUserId().toString();
         ProfileController profileController = new ProfileController();
         String username = profileController.findUserName(userId);
@@ -27,7 +29,3 @@ public class Main {
         jUi.init();
     }
 }
-//HORA, NOMBRE,ICONO
-//La diferencia entre strategy y fachada es lo mismo pero depennde en que lo usas
-//en controller deberia de llamar un metodo
-//
--- src/main/java/edu/upb/chatupb_v2/model/repository/ConnectionDB.java
+++ src/main/java/edu/upb/chatupb_v2/model/repository/ConnectionDB.java
@@ -4,9 +4,11 @@
  */
 package edu.upb.chatupb_v2.model.repository;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -16,6 +18,7 @@ public class ConnectionDB {
 
     private static final ConnectionDB connection = new ConnectionDB();
     private static boolean connectionLogged = false;
+    private static final String URL = getDatabaseURL();
 
     private ConnectionDB() {
     }
@@ -23,12 +26,21 @@ public class ConnectionDB {
     public static ConnectionDB getInstance() {
         return connection;
     }
+    private static String getDatabaseURL() {
+        String appDataPath = System.getenv("APPDATA");
+        File folder = new File(appDataPath + File.separator + "ChatUPB");
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+        String dbPath = folder.getAbsolutePath() + File.separator + "chat_upb_v2.sqlite";
+        return "jdbc:sqlite:" + dbPath;
+    }
 
     public Connection getConection() {
         Connection conn = null;
         try {
             Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite:chat_upb.sqlite");
+            conn = DriverManager.getConnection(URL);
             if (conn != null && !connectionLogged) {
                 connectionLogged = true;
                 System.out.println("Conexion exitosa");
@@ -38,5 +50,44 @@ public class ConnectionDB {
         } catch (ClassNotFoundException e) {
         }
         return conn;
+    }
+    public static void initDataBase(){
+        try (Connection conn = DriverManager.getConnection(URL);
+             Statement stm = conn.createStatement()){
+
+            stm.execute("CREATE TABLE IF NOT EXISTS user_profile (" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "user_id TEXT UNIQUE NOT NULL," +
+                    "user_name TEXT NOT NULL," +
+                    "created_at TEXT," +
+                    "theme_id TEXT DEFAULT 'default'" +
+                    ")");
+
+
+            stm.execute("CREATE TABLE IF NOT EXISTS contact (" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "code TEXT UNIQUE NOT NULL," +
+                    "name TEXT," +
+                    "ip TEXT," +
+                    "theme_id TEXT DEFAULT 'default'" +
+                    ")");
+
+            stm.execute("CREATE TABLE IF NOT EXISTS message (" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "cod_message TEXT," +
+                    "recipient_code TEXT," +
+                    "created_date TEXT," +
+                    "sender_code TEXT," +
+                    "message TEXT," +
+                    "type TEXT," +
+                    "room_code TEXT," +
+                    "status_message TEXT DEFAULT 'SENT'," +
+                    "pinned INTEGER DEFAULT 0" +
+                    ")");
+
+
+        }catch (Exception e){
+
+        }
     }
 }
